a53b33223951825d4932f23d9bfac009d7d9ad08
Jarosław Terejko <j.terejko@gmail.com>
2017-08-24 18:00:27 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+	var carouselList = $('#carousel ul');
+	setInterval(changeSlide, 4000);
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-800}, 2000, moveFirstSlide);
+	};
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+}); // end ready
